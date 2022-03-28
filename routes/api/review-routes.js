23ca7517efc0e51999301343733dbac0c059c995
Legolas <geran.vanatta@gmail.com>
2022-03-28 15:55:57 -0500

    delete route for review-routes

--- routes/api/review-routes.js
+++ routes/api/review-routes.js
@@ -22,3 +22,24 @@ router.post('/', (req, res) => {
             res.status(400).json(err);
         });
 });
+
+router.delete('/:id', (req, res) => {
+    Review.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbReviewData => {
+            if (!dbReviewData) {
+                res.status(404).json({ message: 'No review found with this id!' });
+                return;
+            }
+            res.json(dbReviewData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
