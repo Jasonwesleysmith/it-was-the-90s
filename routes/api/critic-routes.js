217e2c088e1ec5deb8dc2cc6c95254f51592b680
Legolas <geran.vanatta@gmail.com>
2022-03-28 14:32:57 -0500

    update method and delete method routes

--- routes/api/critic-routes.js
+++ routes/api/critic-routes.js
@@ -89,3 +89,42 @@ router.post('login', (req, res) => {
     });
 
 });
+
+router.put('/id:', (req, res) => {
+    Critic.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Critic.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
