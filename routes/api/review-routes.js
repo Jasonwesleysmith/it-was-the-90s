8b88c91a432628fe3efb690cf8376631928afdac
Legolas <geran.vanatta@gmail.com>
2022-03-28 15:47:23 -0500

    post route for review-routes

--- routes/api/review-routes.js
+++ routes/api/review-routes.js
@@ -9,3 +9,16 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/', (req, res) => {
+    Review.create({
+        review_text: req.body.review_text,
+        critic_id: req.body.critic_id,
+        movie_id: req.body.movie_id
+    })
+        .then(dbReviewData => res.json(dbReviewData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
