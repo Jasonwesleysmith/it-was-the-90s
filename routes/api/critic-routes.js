8193f8453ce5b3ab65b9b3c43994a8eda89fd81e
Legolas <geran.vanatta@gmail.com>
2022-03-28 14:26:52 -0500

    require valid login info

--- routes/api/critic-routes.js
+++ routes/api/critic-routes.js
@@ -66,3 +66,26 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('login', (req, res) => {
+    Critic.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+      
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+
+});
