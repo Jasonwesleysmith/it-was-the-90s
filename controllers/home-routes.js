6adc7b040a013cb8d84495dfb91e42e04a250ea1
Legolas <geran.vanatta@gmail.com>
2022-03-29 12:27:58 -0500

    findone get route/ login get route for home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,3 +40,62 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+
+  // get single Movie
+router.get('/movie/:id', (req, res) => {
+    Movie.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'movie_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE movie.id = vote.movie_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Review,
+          attributes: ['id', 'review_text', 'movie_id', 'critic_id', 'created_at'],
+          include: {
+            model: Critic,
+            attributes: ['username']
+          }
+        },
+        {
+          model: Critic,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbMovieData => {
+        if (!dbMovieData) {
+          res.status(404).json({ message: 'No movie found with this id' });
+          return;
+        }
+  
+        const movie = dbMovieData.get({ plain: true });
+  
+        res.render('single-movie', {
+          movie,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
+  
