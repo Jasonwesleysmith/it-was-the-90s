98686eeba710feb05f07014312c3959eaa21abd3
James Murphy <jamesivdeveloper@gmail.com>
2022-03-30 18:22:08 -0500

    add seed.sql and work on review functionality

--- public/javascript/review.js
+++ public/javascript/review.js
@@ -1,29 +1,33 @@
-async function commentFormHandler(event) {
-    event.preventDefault();
-  
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-  
-    if (comment_text) {
-      const response = await fetch('/api/reviews', {
-        method: 'POST',
-        body: JSON.stringify({
-          post_id,
-          comment_text
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      if (response.ok) {
-        document.location.reload();
-      } else {
-        alert(response.statusText);
-      }
+async function reviewFormHandler(event) {
+  event.preventDefault();
+
+  const review_text = document
+    .querySelector('textarea[name="review-body"]')
+    .value.trim();
+  const post_id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  if (review_text) {
+    const response = await fetch("/api/reviews", {
+      method: "POST",
+      body: JSON.stringify({
+        post_id,
+        review_text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
     }
   }
-  
-  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+}
+
+document
+  .querySelector(".review-form")
+  .addEventListener("submit", commentFormHandler);
