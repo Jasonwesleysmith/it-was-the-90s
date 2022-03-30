09ce38a2489216ef9578ab454d30cdf72b943622
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-03-29 19:16:35 -0500

    added review public javascript file

--- public/javascript/add-movie.js
+++ public/javascript/add-movie.js
@@ -5,7 +5,7 @@ async function newFormHandler(event) {
     const post_url = document.querySelector('input[name="movie-url"]').value;
   
     const response = await fetch(`/api/movies`, {
-      method: 'MOVIE',
+      method: 'POST',
       body: JSON.stringify({
         title,
         post_url
